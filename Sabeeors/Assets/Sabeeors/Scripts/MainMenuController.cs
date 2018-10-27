434bdb050148d2d1c3cdbe10e966dcea290cb7f4
Robert <ragr2345@gmail.com>
2018-10-27 19:27:29 -0300

    Menu Principal y controles
    Font de menu
    Funcionalidad de mostrar controles

--- Sabeeors/Assets/Sabeeors/Scripts/MainMenuController.cs
+++ Sabeeors/Assets/Sabeeors/Scripts/MainMenuController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour {
+
+    public GameObject controls;
+    bool active = true; //para que el primer click encienda
+    
     public void Play()
     {
         SceneManager.LoadScene("Level_1");
@@ -15,5 +19,10 @@ public class MainMenuController : MonoBehaviour {
     public void Creditos()
     {
         SceneManager.LoadScene("Creditos");
+    }
+    public void Controles()
+    {
+        controls.SetActive(active);
+        active = !active;
     }
 }
