61ef9335abcc30839d7dd1383c86db62b362e436
alcex <thealcex@gmail.com>
2018-10-27 09:49:45 -0300

    :memo: Added flower script
    Add points to the respective player when they collide and destroys itself.

---
+++ Sabeeors/Assets/Scripts/Flower.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Script for flowers the playes can collide with to gain points.
+/// Flower must be a trigger.
+/// </summary>
+public class Flower : MonoBehaviour {
+
+    public int Points = 1;  // Score obtained by getting this flower.
+
+    private RoundManager roundManager;
+
+    private string playerOneTag;
+    private string playerTwoTag;
+
+	// Use this for initialization
+	void Start () {
+        roundManager = GameObject.Find("RoundManager").GetComponent<RoundManager>();
+
+        if (roundManager == null)
+            Debug.LogError("Round manager not found!");
+
+        playerOneTag = roundManager.PlayerOneTag;
+        playerTwoTag = roundManager.PlayerTwoTag;
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag(playerOneTag))
+            roundManager.AddScore(1, Points);
+        else if (collision.gameObject.CompareTag(playerTwoTag))
+            roundManager.AddScore(2, Points);
+
+        if (collision.gameObject.CompareTag(playerOneTag) || collision.gameObject.CompareTag(playerTwoTag))
+            Destroy(gameObject);
+    }
+}
