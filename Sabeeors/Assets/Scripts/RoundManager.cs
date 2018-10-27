a14cb0011e87fbdf583da5f55fbc26fce4873aab
alcex <thealcex@gmail.com>
2018-10-27 08:41:23 -0300

    :memo: Added skeleton for round manager
    Round manager will handle things like the duration of the round, score of each player, and maybe spawning in the future.

---
+++ Sabeeors/Assets/Scripts/RoundManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+///  Handles the state of the current game round, like player score, round time and random tile generation. 
+/// </summary>
+public class RoundManager : MonoBehaviour {
+
+    public string ResultsScene;  // Name of the scene where the results are shown.
+
+    public float RoundLength;  // Time in seconds.
+    private float roundTimer;
+
+    private int playerOneScore = 0;
+    private int playeTwoScore = 0;
+    
+    // Use this for initialization
+	void Start () {
+        roundTimer = RoundLength;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        roundTimer -= Time.deltaTime;
+
+        if (roundTimer <= 0)
+            SceneManager.LoadScene(ResultsScene);
+	}
+}
