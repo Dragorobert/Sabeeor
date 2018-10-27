7eba82ada08a8a3fc52274f39e1e6269a5f835f0
Ainil Sepulveda <Ainisepulveda@gmail.com>
2018-10-27 14:02:16 -0300

    Bugs menores del Mainmenu y Creditos, con un boton de back
    tambien hice que fueras a creditos eso

---
+++ Sabeeors/Assets/back_script.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class back_script : MonoBehaviour {
+ public void Back()
+    {
+        SceneManager.LoadScene("StartMenu");
+    }
+}
